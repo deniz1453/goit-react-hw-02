42b0bab82e5dda52e7d40eda57e7964e3b8b98d6
deniz1453 <nifamen110@gmail.com>
2025-03-12 22:31:37 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Description from "./components/description/Description";
+import Options from "./components/options/Options";
+import Feedback from "./components/feedback/Feedback";
+
+const App = () => {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem("feedback");
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
+  });
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  return (
+    <>
+      <Description />
+      <Options
+        feedback={feedback}
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+      />
+      <Feedback feedback={feedback} />
+    </>
+  );
+};
+
+export default App;
